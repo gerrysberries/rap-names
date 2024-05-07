7e99efcc44f6847f23f2a99af28dbde17989e618
Gerard Juan <gerardandrejuan@gmail.com>
2024-05-07 16:29:06 -0400

    test

--- api/index.js
+++ api/index.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const app = express();
+const PORT = 8000;
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+const rappers = {
+	'21 savage': {
+		age: 29,
+		birthName: 'Shéyaa Bin Abraham-Joseph',
+		birthLocation: 'London, England',
+	},
+	'chance the rapper': {
+		age: 31,
+		birthName: 'Chancelor Johnathan Bennett',
+		birthLocation: 'Chicago, Illinois, U.S.',
+	},
+	dylan: {
+		age: 31,
+		birthName: 'Dylan',
+		birthLocation: 'Dylan',
+	},
+};
 
-app.listen(8000, () => console.log('Server ready on port 8000.'));
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/:rapperName', (req, res) => {
+	const rapperName = req.params.rapperName.toLowerCase();
+
+	if (rappers[rapperName]) {
+		res.json(rappers[rapperName]);
+	} else {
+		res.json(rappers['dylan']);
+	}
+});
+
+app.get('/api/test/test', (req, res) => {
+	res.json({message: 'Test endpoint works!'});
+});
+
+app.listen(process.env.PORT || PORT, () => {
+	console.log(`Server is running on port ${PORT}!`);
+});
 
 module.exports = app;
